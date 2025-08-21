ac5c21344e088acdbc57bddecca6ece6ba570d5a
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-20 18:18:37 -0600

    Initial commit

---
+++ src/main/java/com/example/markdownnoteapp/MarkdownnoteappApplication.java
@@ -0,0 +1,13 @@
+package com.example.markdownnoteapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkdownnoteappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkdownnoteappApplication.class, args);
+	}
+
+}
