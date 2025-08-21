261b0360a5ce6379b00b2412da3e9f968bf25bfe
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-20 19:04:08 -0600

    Markdown service

--- src/main/java/com/example/markdownnoteapp/service/MarkdownService.java
+++ src/main/java/com/example/markdownnoteapp/service/MarkdownService.java
@@ -1,4 +1,24 @@
 package com.example.markdownnoteapp.service;
 
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+import org.springframework.stereotype.Service;
+
+@Service
 public class MarkdownService {
+    private Parser parser;
+    private HtmlRenderer renderer;
+
+    public MarkdownService () {
+        this.parser = Parser.builder ().build ();
+        this.renderer = HtmlRenderer.builder ().build ();
+    }
+
+    public String convertToHtml (String markdown) {
+        if (markdown == null || markdown.isEmpty ()) {
+            return "";
+        }
+
+        return this.renderer.render (this.parser.parse (markdown));
+    }
 }
