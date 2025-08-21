2b2efb74e340e6da737582ff751de8856fd6228c
James Hepburn <jameshepburn@Jamess-MacBook-Pro.local>
2025-08-20 18:27:50 -0600

    Note entity

--- src/main/java/com/example/markdownnoteapp/model/Note.java
+++ src/main/java/com/example/markdownnoteapp/model/Note.java
@@ -1,4 +1,31 @@
 package com.example.markdownnoteapp.model;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
 public class Note {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String markdownContent;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    public Note (String title, String markdownContent) {
+        this.title = title;
+        this.markdownContent = markdownContent;
+    }
 }
